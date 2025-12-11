8cdb8746be238602ca21bb10a4cd7e99dbbc9104
Abduboriy <134278254+Gammbity@users.noreply.github.com>
2025-12-11 07:48:10 +0500

    Alisher (#7)
    * my updates finished
    
    * updated code
    
    * add  new
    
    * code updated
    
    * add new ui
    
    ---------
    
    Co-authored-by: Yevgenevic <yevgenevic@github.com>

--- menu.py
+++ menu.py
@@ -1,41 +1,42 @@
 import os
-import platform
-from dastur_kodlari.Shahzoda.admin import admin_panel
+from dastur_kodlari.shahzoda.admin import admin_panel
 from dastur_kodlari.alisher.superuser_service import run_superuser_system
-
-KOK = "\033[34m"
+KOK   = "\033[34m"
 QIZIL = "\033[31m"
 YASHIL = "\033[32m"
-RANG = "\033[0m"
+SARIQ = "\033[33m"
+RANG  = "\033[0m"
 
-def clear_screen():
-    if "TERM" not in os.environ:
-        os.environ["TERM"] = "xterm"
-    if platform.system() == "Windows":
-        os.system("cls")
-    else:
-        try:
-            os.system("clear")
-        except Exception:
-            print("\n" * 100)
 
 def warehouse_menu():
     while True:
-        clear_screen()
-        print(f"""
-         {KOK}Welcome to Warehouse
-
-                1. Admin
-                2. SuperUser{RANG}
-        """)
-        tanlov = input(f"{YASHIL}Tanlovni kiriting: {RANG}")
+        os.system("cls" if os.name == "nt" else "clear")
+        print(KOK + r"""
+ _       __  ___    ____    ______   __  __   ____    __  __   _____    ______
+| |     / / /   |  / __ \  / ____/  / / / /  / __ \  / / / /  / ___/   / ____/
+| | /| / / / /| | / /_/ / / __/    / /_/ /  / / / / / / / /   \__ \   / __/   
+| |/ |/ / / ___ |/ _, _/ / /___   / __  /  / /_/ / / /_/ /   ___/ /  / /___   
+|__/|__/ /_/  |_/_/ |_| /_____/  /_/ /_/   \____/  \____/   /____/  /_____/   
+        """ + RANG)
+        
+        print(f"{YASHIL}=== TIZIM BOSHQARUVI ==={RANG}")
+        print(f"1. {KOK}Admin Panel{RANG}")
+        print(f"2. {SARIQ}SuperUser Panel{RANG}")
+        print(f"0. {QIZIL}Chiqish{RANG}")
+        
+        tanlov = input(f"\n{YASHIL}>>> Tanlovni kiriting: {RANG}")
+        
         if tanlov == "1":
             admin_panel()
         elif tanlov == "2":
             run_superuser_system()
+        elif tanlov == "0":
+            print("Xayr, salomat bo'ling!")
+            break
         else:
             print(f"{QIZIL}Noto'g'ri tanlov!!!{RANG}")
-            input(f"{QIZIL}Boshidan tanlang. Enter bosish orqali davom eting.{RANG}")
+            input("Enter...")
+
 
 if __name__ == "__main__":
     warehouse_menu()
